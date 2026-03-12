72becb0c80e7d9d08d0524568f4e77bc44c4598a
DG-IT2024 <160723812+DG-IT2024@users.noreply.github.com>
2026-03-12 20:22:02 +0800

    journalV1

--- app/src/main/java/com/example/myapplication/data/local/AppDatabase.java
+++ app/src/main/java/com/example/myapplication/data/local/AppDatabase.java
@@ -11,7 +11,7 @@ import com.example.myapplication.data.local.entity.*;
 
 @Database(
         entities = {JournalEntryEntity.class, VideoEntity.class, HotlineEntity.class},
-        version = 8,
+        version = 9,
         exportSchema = true
 )
 
